9dadd1d4b4bbf4ce6ad7a21a13c0e2510a20ceed
Faisal Zamir <jafri.site@gmail.com>
2025-10-01 15:07:43 +0500

    Fixed again tkinter issue

--- TaxiPriceMLProj/wsgi.py
+++ TaxiPriceMLProj/wsgi.py
@@ -1,12 +1,21 @@
+import sys
 import os
-os.environ['MPLBACKEND'] = 'Agg'
 
-# If using matplotlib anywhere
-try:
-    import matplotlib
-    matplotlib.use('Agg')
-except ImportError:
-    pass
+# Debug: Track imports
+original_import = __builtins__.__import__
+
+def debug_import(name, *args, **kwargs):
+    if 'tkinter' in name or '_tkinter' in name:
+        print(f"ATTEMPTING TO IMPORT: {name}")
+        print("Stack trace:")
+        import traceback
+        traceback.print_stack()
+    return original_import(name, *args, **kwargs)
+
+__builtins__.__import__ = debug_import
+
+# Now set backend
+os.environ['MPLBACKEND'] = 'Agg'
 
 
 from django.core.wsgi import get_wsgi_application
